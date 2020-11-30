075363130f85057167b1ca73b68dd49eca75cbdd
Andrea Malleo <akmalleo3@gmail.com>
2020-11-29 21:56:00 -0500

    it's a bench!

--- MarchingCubes/src/main.cpp
+++ MarchingCubes/src/main.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-  MarchingCubes::assembleCubes("/home/andrea/Documents/GradSchool/OccupancyNetworks/nyu_occupancy_networks_project/coords.txt",
-                               "/home/andrea/Documents/GradSchool/OccupancyNetworks/nyu_occupancy_networks_project/predictions.txt");
+  std::vector<Cube> cubes = MarchingCubes::assembleCubes("/home/andrea/Documents/GradSchool/OccupancyNetworks/nyu_occupancy_networks_project/evaluation/coords.txt",
+                                                         "/home/andrea/Documents/GradSchool/OccupancyNetworks/nyu_occupancy_networks_project/evaluation/predictions.txt");
+
+  MarchingCubes::march(cubes, "/home/andrea/Documents/GradSchool/OccupancyNetworks/nyu_occupancy_networks_project/MarchingCubes/mesh.off");
   
   return 0;
 }
--- MarchingCubes/src/marchingCubes.h
+++ MarchingCubes/src/marchingCubes.h
@@ -4,11 +4,25 @@
 
 class Vertex{
   public:
-  Vertex(float xx,float yy, float zz): x(xx), y(yy), z(zz){};
+  Vertex(float xx,float yy, float zz): x(xx), y(yy), z(zz){globalIdx = -1;};
   float x;
   float y;
   float z;
   bool occupies;
+  int globalIdx;
+  bool operator<(const Vertex &other) const{
+      if ((x-other.x) > -0.001) return true;
+      if ((x-other.x) > 0.001) return false;
+      if ((y-other.y) > -0.001) return true;
+      if ((z-other.y) > 0.001) return false;
+      if ((z-other.z) > -0.001) return true;
+      return false;
+  }
+  bool operator==(const Vertex &other) const{
+    if ((abs(x-other.x) < 0.001) && (abs(y-other.y) < 0.001) && (abs(z-other.z) < 0.001))
+      return true;
+    return false;
+  }
 };
 
 class Cube{
@@ -16,6 +30,13 @@ public:
 
   std::vector<Vertex> vertices;
   
+};
+
+
+struct Face{
+  int v1;
+  int v2;
+  int v3;
 };
 
 class MarchingCubes{
