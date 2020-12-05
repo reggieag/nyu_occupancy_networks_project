a41d4c073db33645ba531ef755ac7166465851a2
Reggie Gomez <reggie.gomez@betterment.com>
2020-12-05 17:40:25 -0500

    add data loading

--- training/train_occupancy_model.py
+++ training/train_occupancy_model.py
@@ -1,32 +1,34 @@
 import torch
-import io
+import os
 
 from utils.data_loader import DataSetClass
 from utils.constants import K, BATCH_SIZE, DEVICE
 
-topdir = "/home/andrea/Documents/GradSchool/OccupancyNetworks/occupancy_networks"
-couchesDirectory = f"{topdir}/data/ShapeNet/04256520"
+SHAPENET_DIR = "/scratch/rag551/occupancy_networks/ShapeNet"
 
+# SHAPNET_CLASSES = [
+#     '02958343'
+# ]
+
+SHAPENET_CLASS = '02958343'
+
+
+def load_list_dirs(dir, list_file):
+    with open(os.path.join(dir, list_file)) as train_list:
+        return [train_dir for train_dir in train_list.readlines()]
+
+
+def generate_data_loader(dir, list_file):
+    dirs = load_list_dirs(dir, list_file)
+    datasets = [DataSetClass(dir) for dir in dirs]
+    data = torch.utils.data.ConcatDataset(datasets)
+    return torch.utils.data.DataLoader(data, batch_size=BATCH_SIZE, shuffle=True)
 
 if __name__ == '__main__()':
+    shapenet_class_dir = os.path.join(SHAPENET_DIR, SHAPENET_CLASS)
 
     # catalogue all of the directories with the chosen category
-    trainingDirs = []
-    with io.open(f"{couchesDirectory}/train.lst") as trainlist:
-        for traindir in trainlist.readlines():
-            trainingDirs.append(f"{couchesDirectory}/{traindir.strip()}")
-    dataSets = []
-    for tdir in trainingDirs:
-        dataSets.append(DataSetClass(tdir))
-    data = torch.utils.data.ConcatDataset(dataSets)
-    train_loader = torch.utils.data.DataLoader(data, batch_size=BATCH_SIZE, shuffle=True)
+    train_loader = generate_data_loader(shapenet_class_dir, 'train.lst')
+
     # Get the validation data
-    valDirs = []
-    with io.open(f"{couchesDirectory}/val.lst") as vallist:
-        for valdir in vallist.readlines():
-            valDirs.append(f"{couchesDirectory}/{valdir.strip()}")
-    dataSets = []
-    for vdir in valDirs:
-        dataSets.append(DataSetClass(vdir))
-    val_data = torch.utils.data.ConcatDataset(dataSets)
-    val_loader = torch.utils.data.DataLoader(val_data, batch_size=BATCH_SIZE, shuffle=True)
+    validation_loader = generate_data_loader(shapenet_class_dir, 'val.lst')
