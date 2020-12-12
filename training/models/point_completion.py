a23c7ec825fab7e56d0d236ac6b77bb02fa22464
Reggie Gomez <reggie.gomez@betterment.com>
2020-12-12 16:30:50 -0500

    add squeeze

--- training/models/point_completion.py
+++ training/models/point_completion.py
@@ -83,7 +83,7 @@ class PointNetEncoder(nn.Module):
         # print(f"k is {k}")
         x = F.max_pool1d(x, k)
 
-        # x = x.squeeze()
+        x = x.squeeze()
         print(f"x.shape is {x.shape}")
         pts = self.fc_final(x)
 
