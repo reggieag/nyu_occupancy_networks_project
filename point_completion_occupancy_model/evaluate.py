5f15e975600646533667647ac37e0b8b1f9936e7
Reggie Gomez <reggie.gomez@betterment.com>
2020-12-13 12:57:45 -0500

    more prints

--- point_completion_occupancy_model/evaluate.py
+++ point_completion_occupancy_model/evaluate.py
@@ -87,9 +87,12 @@ def generate_adaptive_grid(ncuts, xl, xh, yl, yh, zl, zh, limit, mesh_funct, onC
 
 
 def over_model_threshold(model, point_cloud, pt):
-    # print(pt)
-    # print(pt.view(-1, 1, 3, 1))
-    # print(pt.view(-1, 1, 3, 1).permute(0, 2, 1, 3))
+    print(pt)
+    print(pt.view(-1, 1, 3, 1))
+    print(pt.view(-1, 1, 3, 1).permute(0, 2, 1, 3))
+    print(pt.view(-1, 1, 3, 1).permute(0, 2, 1, 3).shape)
+
+    print(point_cloud.shape)
     x = model(pt.view(-1, 1, 3, 1).permute(0, 2, 1, 3), point_cloud)
     # print(x)
     # print(x[0])
