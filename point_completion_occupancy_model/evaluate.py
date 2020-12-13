1647457d8ec0a6a80c03a25a32cb289d09b109a2
Reggie Gomez <reggie.gomez@betterment.com>
2020-12-13 16:51:58 -0500

    try evaluate

--- point_completion_occupancy_model/evaluate.py
+++ point_completion_occupancy_model/evaluate.py
@@ -128,15 +128,13 @@ if __name__ == "__main__":
         # print(pts.shape)
         # print(occupancies.shape)
         # print(org_pointcloud.shape)
-        write_point_cloud_to_xyz(org_pointcloud, 'original_point_cloud.xyz')
-        write_point_cloud_to_xyz(sample_pointcloud, 'sample_point_cloud.xyz')
+        write_point_cloud_to_xyz(org_pointcloud, 'original_point_cloud_2.xyz')
+        write_point_cloud_to_xyz(sample_pointcloud, 'sample_point_cloud_2.xyz')
         sample_pointcloud = sample_pointcloud.view(-1, POINTCLOUD_N, 3, 1).permute(0, 2, 1, 3).cuda()
 
-        # f = partial(over_model_threshold, model, sample_pointcloud)
-        #
-        # g = generate_adaptive_grid(32, -0.5, 0.5, -0.5, 0.5, -0.5, 0.5, 3, f, True)
-        #
-        # numpy.savetxt('electronic_ag_32_3.txt', g.detach().numpy())
-        # numpy.savetxt('incomplete_sample_pointcloud.txt', sample_pointcloud.detach().numpy())
+        f = partial(over_model_threshold, model, sample_pointcloud)
+        g = generate_adaptive_grid(32, -0.5, 0.5, -0.5, 0.5, -0.5, 0.5, 3, f, True)
+
+        numpy.savetxt('electronic_ag_32_3.txt', g.detach().numpy())
 
         break
